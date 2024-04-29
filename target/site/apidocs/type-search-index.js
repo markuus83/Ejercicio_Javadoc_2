19ee80ee0667ec869c072ea788d98b77271ef894
Marcos <a23marcosCC@ies.sanclemente.net>
2024-04-29 09:26:38 +0200

    Seguimiento de las clases correctas, problemas solucionados

--- target/site/apidocs/type-search-index.js
+++ target/site/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"estudiante","l":"App"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"estudiante","l":"Estudiante"}];updateSearchResults();
